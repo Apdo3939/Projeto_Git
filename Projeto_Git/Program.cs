c8f264e63d75e95d0c9861e3462fed0d5f9bdc83
Alexandre Pereira <apdo3939@hotmail.com>
2018-01-14 16:27:29 -0200

    Git_Versão_00

---
+++ Projeto_Git/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_Git
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            Console.WriteLine("\t-----------------------------------------");
+            Console.WriteLine("\t---------------- TESTE ------------------");
+            Console.WriteLine("\t-----------------------------------------");
+            Console.WriteLine();
+            Console.WriteLine("\tPrograma de teste para o repositório GIT_Versão_00.");
+            Console.ReadLine();
+        }
+    }
+}
